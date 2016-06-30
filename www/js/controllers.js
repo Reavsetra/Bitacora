ec50f77d0984eaddcb259005aec3dde7cea95ac2
Emmanuel Navarro <reavsetra@live.com>
2016-06-30 16:26:48 -0500

    Error

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,36 +1,6 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $http, $ionicPopup) {
-
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  // Form data for the login modal
-  // $scope.loginData = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when  the user submits the login form
-
 })
 
 .controller('BitacoraCtrl', function($scope, $http, $ionicModal,$ionicPopup, $rootScope) {
@@ -73,6 +43,7 @@ angular.module('starter.controllers', [])
     console.log($scope.actividades);
   });
 })
+
 .controller('ActividadCtrl', function($scope, $http, $state, $stateParams, $ionicModal,$ionicPopup) {
   //Funcion para encontrar el index del usuario a buscar
   function encontrarIndexArray(array, attr, value) {
